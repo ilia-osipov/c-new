5fe4cadb525ce1f8ed210cccf8311c3f4b148c4c
Marina <marinosharipo@gmail.com>
2022-08-20 19:42:22 +0700

    added if-else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "илья")
+{
+    Console.WriteLine("Ура, это же ИЛЬЯ!!!!!!!!!!!!!!!111111");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
